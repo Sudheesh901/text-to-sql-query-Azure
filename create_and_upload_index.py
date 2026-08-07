3eaa57e9147c117e1e89dc3d7ca31cd60a35bed4
Sudheesh901 <sudheeshkrj@gmail.com>
2026-08-07 12:11:08 +0530

    feat(search): add Azure AI Search index with text-embedding-3-small

---
+++ create_and_upload_index.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from openai import AzureOpenAI
+
+from azure.core.credentials import AzureKeyCredential
+from azure.search.documents.indexes import SearchIndexClient
+from azure.search.documents.indexes.models import SearchIndex, SimpleField,SearchableField
+from azure.search.documents import SearchClient
+load_dotenv()
+
+endpoint=os.getenv("AZURE_SEARCH_ENDPOINT")
+key=os.getenv("AZURE_SEARCH_KEY")
+index_name=os.getenv("INDEX_NAME")
+deployment=os.getenv("TEXT_EMBEDDING_MODEL")
+api_version=os.getenv("SEARCH_API_VERSION")
+
+client=SearchIndexClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(key)
+)
+
+if index_name in [idx.name for idx in client.list_indexes()]:
+    client.delete_index(index_name)
+
+# Define Fields
+
+fields = [
+    SimpleField(name="id", type="Edm.String", key=True, searchable=False),
+    SearchableField(name="type", type="Edm.String", filterable=True, searchable=True, analyzer_name="standard.lucene"),
+    SearchableField(name="name", type="Edm.String", sortable=True, analyzer_name="standard.lucene"),
+    SearchableField(name="description", type="Edm.String", sortable=True, analyzer_name="standard.lucene"),
+    SearchableField(name="columns", type="Edm.String", sortable=True, analyzer_name="standard.lucene"),
+]
+
+# Create Index
+index= SearchIndex(name=index_name, fields=fields)
+client.create_index(index)
+print(f"Created Index: {index_name}")
+
